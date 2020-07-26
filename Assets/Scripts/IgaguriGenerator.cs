6a3317b1fd7a20c689c0182531e6d95d50a253c1
Daichi <55573165+S-DaichiPG@users.noreply.github.com>
2020-07-26 14:05:04 +0900

    dev-v01
    サンプルゲームの仕様が完成（ここからアレンジを加えていく）

--- Assets/Scripts/IgaguriGenerator.cs
+++ Assets/Scripts/IgaguriGenerator.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class IgaguriGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject igaguriPrefab;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject igaguri = Instantiate(igaguriPrefab) as GameObject;
+            
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            igaguri.GetComponent<IgaguriController>().Shoot(
+                worldDir.normalized*2000);
+
+        }
     }
 }
